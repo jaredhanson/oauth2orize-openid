8864f7ad545359ed97b58eeae1aeef13bab8ed55
Jared Hanson <jaredhanson@gmail.com>
2013-09-23 08:23:47 -0700

    More test cases for parsing request extensions.

--- test/request/extensions.test.js
+++ test/request/extensions.test.js
@@ -69,6 +69,151 @@ describe('authorization request extensions', function() {
       });
     });
     
+    describe('request without parameters', function() {
+      var err, ext;
+      
+      before(function(done) {
+        chai.grant(extensions())
+          .req(function(req) {
+            req.query = {};
+          })
+          .parse(function(e, o) {
+            err = e;
+            ext = o;
+            done();
+          })
+          .authorize();
+      });
+      
+      it('should not error', function() {
+        expect(err).to.be.null;
+      });
+      
+      it('should parse request', function() {
+        expect(ext.display).to.equal('page');
+        expect(ext.maxAge).to.be.undefined;
+        expect(ext.uiLocales).to.be.undefined;
+        expect(ext.claimsLocales).to.be.undefined;
+        expect(ext.acrValues).to.be.undefined;
+      });
+    });
+    
+    describe('request with multiple prompts', function() {
+      var err, ext;
+      
+      before(function(done) {
+        chai.grant(extensions())
+          .req(function(req) {
+            req.query = {};
+            req.query.prompt = 'login consent';
+          })
+          .parse(function(e, o) {
+            err = e;
+            ext = o;
+            done();
+          })
+          .authorize();
+      });
+      
+      it('should not error', function() {
+        expect(err).to.be.null;
+      });
+      
+      it('should parse request', function() {
+        expect(ext.prompt).to.be.an('array');
+        expect(ext.prompt).to.have.length(2);
+        expect(ext.prompt[0]).to.equal('login');
+        expect(ext.prompt[1]).to.equal('consent');
+      });
+    });
+    
+    describe('request with multiple UI locales', function() {
+      var err, ext;
+      
+      before(function(done) {
+        chai.grant(extensions())
+          .req(function(req) {
+            req.query = {};
+            req.query.ui_locales = 'en es';
+          })
+          .parse(function(e, o) {
+            err = e;
+            ext = o;
+            done();
+          })
+          .authorize();
+      });
+      
+      it('should not error', function() {
+        expect(err).to.be.null;
+      });
+      
+      it('should parse request', function() {
+        expect(ext.uiLocales).to.be.an('array');
+        expect(ext.uiLocales).to.have.length(2);
+        expect(ext.uiLocales[0]).to.equal('en');
+        expect(ext.uiLocales[1]).to.equal('es');
+      });
+    });
+    
+    describe('request with multiple claims locales', function() {
+      var err, ext;
+      
+      before(function(done) {
+        chai.grant(extensions())
+          .req(function(req) {
+            req.query = {};
+            req.query.claims_locales = 'en es';
+          })
+          .parse(function(e, o) {
+            err = e;
+            ext = o;
+            done();
+          })
+          .authorize();
+      });
+      
+      it('should not error', function() {
+        expect(err).to.be.null;
+      });
+      
+      it('should parse request', function() {
+        expect(ext.claimsLocales).to.be.an('array');
+        expect(ext.claimsLocales).to.have.length(2);
+        expect(ext.claimsLocales[0]).to.equal('en');
+        expect(ext.claimsLocales[1]).to.equal('es');
+      });
+    });
+    
+    describe('request with multiple ACR values', function() {
+      var err, ext;
+      
+      before(function(done) {
+        chai.grant(extensions())
+          .req(function(req) {
+            req.query = {};
+            req.query.acr_values = '2 1';
+          })
+          .parse(function(e, o) {
+            err = e;
+            ext = o;
+            done();
+          })
+          .authorize();
+      });
+      
+      it('should not error', function() {
+        expect(err).to.be.null;
+      });
+      
+      it('should parse request', function() {
+        expect(ext.acrValues).to.be.an('array');
+        expect(ext.acrValues).to.have.length(2);
+        expect(ext.acrValues[0]).to.equal('2');
+        expect(ext.acrValues[1]).to.equal('1');
+      });
+    });
+    
     describe('request with prompt including none with other values', function() {
       var err, ext;
       
