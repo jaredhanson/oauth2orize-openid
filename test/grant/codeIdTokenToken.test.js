c793bb054cac1959dbcd96c7889ea348f2fa2c08
Jared Hanson <jaredhanson@gmail.com>
2015-10-29 15:13:30 -0700

    Add request parsing test case.

--- test/grant/codeIdTokenToken.test.js
+++ test/grant/codeIdTokenToken.test.js
@@ -35,4 +35,41 @@ describe('grant.codeIdTokenToken', function() {
     }).to.throw(TypeError, 'oauth2orize-openid.codeIDTokenToken grant requires an issueIDToken callback');
   });
   
+  describe('request parsing', function() {
+    function issueToken(){}
+    function issueCode(){}
+    function issueIDToken(){}
+    
+    describe('request', function() {
+      var err, out;
+      
+      before(function(done) {
+        chai.oauth2orize.grant(codeIdTokenToken(issueToken, issueCode, issueIDToken))
+          .req(function(req) {
+            req.query = {};
+            req.query.client_id = 'c123';
+            req.query.redirect_uri = 'http://example.com/auth/callback';
+            req.query.state = 'f1o1o1';
+          })
+          .parse(function(e, o) {
+            err = e;
+            out = o;
+            done();
+          })
+          .authorize();
+      });
+      
+      it('should not error', function() {
+        expect(err).to.be.null;
+      });
+      
+      it('should parse request', function() {
+        expect(out.clientID).to.equal('c123');
+        expect(out.redirectURI).to.equal('http://example.com/auth/callback');
+        expect(out.scope).to.be.undefined;
+        expect(out.state).to.equal('f1o1o1');
+      });
+    });
+  });
+  
 });
